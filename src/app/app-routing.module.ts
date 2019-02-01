e1dbe85d8693d0c498df2b3f2662d2b9bc4d33ef
ritvik <ritvik.mandyam@gmail.com>
2019-02-01 11:05:44 -0800

    Added all the things!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {NoteViewComponent} from './note-view/note-view.component';
+import {NotesListComponent} from './notes-list/notes-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'note', component: NoteViewComponent},
+  {path: 'note/:id', component: NoteViewComponent},
+  {path: 'notes', component: NotesListComponent},
+  { path: '', redirectTo: '/note', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,44 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatIconModule} from '@angular/material/icon';
+import {HotkeyModule} from 'angular2-hotkeys';
+import {ContenteditableModule} from 'ng-contenteditable';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {RecallNotePairComponent} from './recall-note-pair/recall-note-pair.component';
+import {NoteTakingToolbarComponent} from './note-taking-toolbar/note-taking-toolbar.component';
+import {MatButtonModule, MatCardModule, MatTooltipModule} from '@angular/material';
+import { NoteViewComponent } from './note-view/note-view.component';
+import { NotesListComponent } from './notes-list/notes-list.component';
+import { NoteTileComponent } from './note-tile/note-tile.component';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+import { TextListComponent } from './text-list/text-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecallNotePairComponent,
+    NoteTakingToolbarComponent,
+    NoteViewComponent,
+    NotesListComponent,
+    NoteTileComponent,
+    TextListComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    HotkeyModule.forRoot(),
+    ContenteditableModule,
+    MatButtonModule,
+    MatCardModule,
+    MatTooltipModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
