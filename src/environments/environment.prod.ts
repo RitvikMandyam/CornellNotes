0ecfcd93fb794c51952c2b2f8c9718296518c69d
ritvik <ritvik.mandyam@gmail.com>
2019-02-06 16:33:08 -0800

    Added docs directory for publishing

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAFiVVxtHVW9nCEFDLPdA6v_Ug5gAqFGtA',
+    authDomain: 'cornell-notes-backend.firebaseapp.com',
+    databaseURL: 'https://cornell-notes-backend.firebaseio.com',
+    projectId: 'cornell-notes-backend',
+    storageBucket: 'cornell-notes-backend.appspot.com',
+    messagingSenderId: '459546275907'
+  }
 };
